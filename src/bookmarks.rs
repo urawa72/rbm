fb5c5f87a578d6cecfe04b2ed4daa9a70827df41
urawa72 <urawa72@gmail.com>
2021-12-08 21:44:02 +0900

    impl Add, List command

--- src/bookmarks.rs
+++ src/bookmarks.rs
@@ -1,13 +1,36 @@
-use std::io::{self, BufRead, Error, stdout, Write};
+use serde::{Deserialize, Serialize};
+use core::fmt;
+use std::{
+    fs::{File, OpenOptions},
+    io::{stdin, stdout, BufRead, Error, Seek, SeekFrom, Write},
+    path::PathBuf,
+};
 
-#[derive(Debug)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct Bookmark {
     title: String,
     url: String,
 }
 
-pub fn add_bookmark() -> Result<(), Error> {
-    let reader = io::stdin();
+impl fmt::Display for Bookmark {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "[{}]({})", self.title, self.url)
+    }
+}
+
+fn collect_bookmarks(mut file: &File) -> Result<Vec<Bookmark>, Error> {
+    file.seek(SeekFrom::Start(0))?;
+    let bookmarks: Vec<Bookmark> = match serde_json::from_reader(file) {
+        Ok(bookmarks) => bookmarks,
+        Err(e) if e.is_eof() => Vec::new(),
+        Err(e) => Err(e)?,
+    };
+    file.seek(SeekFrom::Start(0))?;
+    Ok(bookmarks)
+}
+
+pub fn add_bookmark(journal_file: PathBuf) -> Result<(), Error> {
+    let reader = stdin();
     let mut handle = reader.lock();
 
     let mut title = String::new();
@@ -20,15 +43,35 @@ pub fn add_bookmark() -> Result<(), Error> {
     stdout().flush()?;
     handle.read_line(&mut url)?;
 
-    println!(
-        "title = {} url = {}",
-        title.trim().to_string(),
-        url.trim().to_string()
-    );
+    let bookmark = Bookmark {
+        title: title.trim().to_string(),
+        url: url.trim().to_string()
+    };
+
+    let file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(journal_file)?;
+
+    let mut bookmarks = collect_bookmarks(&file)?;
+    bookmarks.push(bookmark);
+    serde_json::to_writer(file, &bookmarks)?;
+
     Ok(())
 }
 
-pub fn list_bookmarks() -> Result<(), Error> {
-    println!("Hello, CLI World!!!");
+pub fn list_bookmarks(journal_file: PathBuf) -> Result<(), Error> {
+    let file = OpenOptions::new().read(true).open(journal_file)?;
+    let bookmarks = collect_bookmarks(&file)?;
+    if bookmarks.is_empty() {
+        println!("Bookmark list is empty!");
+    } else {
+        let mut order: u32 = 1;
+        for bookmark in bookmarks.iter() {
+            println!("{}: {}", order, bookmark);
+            order += 1;
+        }
+    }
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,26 @@
+use std::path::PathBuf;
+
 use cli::{Action::*, CommandLineArgs};
 use structopt::StructOpt;
 
 mod bookmarks;
 mod cli;
 
+fn find_journal_file() -> Option<PathBuf> {
+    home::home_dir().map(|mut path| {
+        path.push(".rusty-bookmarks.json");
+        path
+    })
+}
+
 fn main() -> anyhow::Result<()> {
     let CommandLineArgs { action } = CommandLineArgs::from_args();
 
+    let journal_file = find_journal_file().expect("Failed to find journal file");
+
     match action {
-        Add  => bookmarks::add_bookmark(),
-        List => bookmarks::list_bookmarks(),
+        Add => bookmarks::add_bookmark(journal_file),
+        List => bookmarks::list_bookmarks(journal_file),
     }?;
     Ok(())
 }
