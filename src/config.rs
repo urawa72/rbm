f675bfdf0b5dd0354bceef4ec63a4470855f8149
urawa72 <urawa72@gmail.com>
2021-12-16 17:42:27 +0900

    load exist config file correctly

--- src/config.rs
+++ src/config.rs
@@ -8,8 +8,45 @@ use serde::Deserialize;
 use serde::Serialize;
 
 #[derive(Debug, Serialize, Deserialize)]
-struct Config {
-    path: PathBuf,
+struct ConfigFile {
+    config: Config,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Config {
+    pub path: PathBuf,
+}
+
+pub fn find_config_file() -> Option<PathBuf> {
+    home::home_dir().map(|mut path| {
+        path.push("rbm-config.toml");
+        path
+    })
+}
+
+pub fn load_config(config_file_path: PathBuf) -> Result<Config, Box<dyn std::error::Error>> {
+    let toml = read_to_string(config_file_path.clone());
+    let toml = match toml {
+        Ok(toml) => toml,
+        Err(_) => {
+            // Create new config file if not exist
+            let journal_file_path = find_journal_file().expect("Failed to find journal file");
+            let config = Config { path: journal_file_path };
+            let config_file = ConfigFile { config };
+            let toml = toml::to_string(&config_file).expect("Failed to to_string config");
+            let mut f = OpenOptions::new()
+                .create(true)
+                .write(true)
+                .open(config_file_path)?;
+            write!(f, "{}", toml.clone())?;
+            toml
+        }
+    };
+    let config_file: Result<ConfigFile, toml::de::Error> = toml::from_str(&toml);
+    match config_file {
+        Ok(config_file) => Ok(config_file.config),
+        Err(e) => panic!("Failed to load toml: {}", e),
+    }
 }
 
 fn find_journal_file() -> Option<PathBuf> {
@@ -19,25 +56,3 @@ fn find_journal_file() -> Option<PathBuf> {
     })
 }
 
-pub fn load_config(config_file: PathBuf) -> Result<PathBuf, Box<dyn std::error::Error>> {
-    let toml = read_to_string(config_file.clone());
-    let toml = match toml {
-        Ok(toml) => toml,
-        Err(_) => {
-            let journal_file = find_journal_file().expect("Failed to find journal file");
-            let config = Config { path: journal_file };
-            let toml = toml::to_string(&config).expect("Failed to to_string config");
-            let mut f = OpenOptions::new()
-                .create(true)
-                .write(true)
-                .open(config_file)?;
-            write!(f, "[config]\n{}", toml.clone())?;
-            toml
-        }
-    };
-    let config: Result<Config, toml::de::Error> = toml::from_str(&toml);
-    match config {
-        Ok(config) => Ok(config.path),
-        Err(e) => panic!("Failed to load toml: {}", e),
-    }
-}
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,17 @@
-use std::path::PathBuf;
-use rbm::config::load_config;
+use rbm::config::{find_config_file, load_config};
 use structopt::StructOpt;
 use rbm::bookmarks::{add_bookmark, list_bookmarks};
 use rbm::cli::{Action::*, CommandLineArgs};
 
-fn find_config_file() -> Option<PathBuf> {
-    home::home_dir().map(|mut path| {
-        path.push("rbm-config.toml");
-        path
-    })
-}
-
 fn main() -> anyhow::Result<()> {
     let CommandLineArgs { action } = CommandLineArgs::from_args();
 
-    let config_file = find_config_file().expect("Failed to find config file");
-    let journal_file = load_config(config_file).expect("Failed to load journal file");
+    let config_file_path = find_config_file().expect("Failed to find config file");
+    let config = load_config(config_file_path).expect("Failed to load config file");
 
     match action {
-        Add => add_bookmark(journal_file),
-        List => list_bookmarks(journal_file),
+        Add => add_bookmark(config.path),
+        List => list_bookmarks(config.path),
     }?;
     Ok(())
 }
